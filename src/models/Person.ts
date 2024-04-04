ea4764d37cbd2bff2b753ad6e796e2164aa7c126
Éverton Carvalho <evertonsnake@gmail.com>
2024-04-04 14:27:04 -0300

    feat: birthDay

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { format, parse, differenceInDays, addYears, isToday, differenceInYears, intervalToDuration } from 'date-fns';
 import { addressSchema, IAddresses } from "./Addresses";
 
 export const personSchema = z.object({
@@ -24,13 +25,24 @@ export class PersonDTO {
   dateOfBirth: Date;
   maritalStatus: string;
   addresses?: IAddresses[];
+  birthdayMessage: string;
+  daysUntilNextBirthday: number;
+  currentAge: number;
 
   constructor(person: IPerson) {
+    const today = new Date();
+    const age = differenceInYears(today, person.dateOfBirth);
+    const nextBirthday = addYears(person.dateOfBirth, age);
+    const daysUntilBirthday = differenceInDays(nextBirthday, today);
+
     this.id = person.id;
     this.name = person.name;
     this.sex = person.sex;
     this.dateOfBirth = person.dateOfBirth;
     this.maritalStatus = person.maritalStatus;
     this.addresses = person.addresses;
+    this.currentAge = age;
+    this.daysUntilNextBirthday = isToday(person.dateOfBirth) ? 0 : daysUntilBirthday;
+    this.birthdayMessage = this.daysUntilNextBirthday === 0 ? "Feliz aniversário!" : `Falta${daysUntilBirthday === 1 ? '' : 'm'} ${Math.abs(daysUntilBirthday)} dia${daysUntilBirthday === 1 ? '' : 's'} para o seu aniversário.`;
   }
 }
